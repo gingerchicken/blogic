a36e9f7ce1b50bce4ed7b3a350ecbc96223470c5
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-10-16 01:17:05 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='blogic',
+    version='0.1',
+    description='Basic boolean algebra enumerator',
+    author='HowITsDone',
+    author_email='32576907+gingerchicken@users.noreply.github.com',
+    packages=['blogic'],
+    install_requires=[]
+)
